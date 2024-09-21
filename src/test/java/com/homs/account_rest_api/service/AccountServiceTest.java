dd2fd31e68c5668ace715e22a635fc9a4a507461
homsdev <obr_dev@protonmail.com>
2024-09-21 11:46:37 -0600

    Completed unit test for accounts service

--- src/test/java/com/homs/account_rest_api/service/AccountServiceTest.java
+++ src/test/java/com/homs/account_rest_api/service/AccountServiceTest.java
@@ -5,21 +5,18 @@ import static org.junit.Assert.*;
 
 import com.homs.account_rest_api.model.Account;
 import com.homs.account_rest_api.repository.AccountRepository;
-import com.homs.account_rest_api.repository.impl.AccountMysqlRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.*;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,15 +34,114 @@ public class AccountServiceTest {
     }
 
     @Test
-    public void name() {
-        List<Account> expectedResult = Arrays.asList(new Account(),new Account());
+    public void findAllShouldReturnAListOfAccounts() {
+        List<Account> expectedResult = Arrays.asList(new Account(), new Account());
 
         when(accountRepository.findAll()).thenReturn(expectedResult);
 
         List<Account> actualResult = accountService.findAll();
 
-        assertEquals(expectedResult,actualResult);
-        assertEquals(expectedResult.size(),actualResult.size());
-        verify(accountRepository,atMostOnce()).findAll();
+        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult.size(), actualResult.size());
+        verify(accountRepository, atMostOnce()).findAll();
+    }
+
+    @Test
+    public void saveAccountShouldSaveNewAccountWhenRepositoryOk() {
+        Account expectedResult = Account.builder()
+                .accountId("f048c183-066c-4906-a0be-e5fe08dccd6b")
+                .balance(BigDecimal.valueOf(10_000.00))
+                .alias("New Account").build();
+
+        when(accountRepository.save(any(Account.class)))
+                .thenReturn(Optional.of(expectedResult));
+
+        Account actualResult = accountService.saveAccount(expectedResult);
+
+        assertEquals(expectedResult, actualResult);
+        verify(accountRepository, atMostOnce()).save(eq(expectedResult));
+    }
+
+    @Test
+    public void findByIdShouldReturnAnAccountOnSuccess() {
+        String id = "f048c183-066c-4906-a0be-e5fe08dccd6b";
+        Account expectedResult = Account.builder()
+                .accountId("f048c183-066c-4906-a0be-e5fe08dccd6b")
+                .balance(BigDecimal.valueOf(10_000.00))
+                .alias("New Account").build();
+
+        when(accountRepository.findById(eq(id)))
+                .thenReturn(Optional.of(expectedResult));
+
+        Account actualResult = accountService.findById(id);
+
+        assertEquals(expectedResult, actualResult);
+        verify(accountRepository, atMostOnce()).findById(eq(id));
+    }
+
+    @Test
+    public void deleteByIdShouldReturnMoreThan1AffectedRow() {
+        String id = "f048c183-066c-4906-a0be-e5fe08dccd6b";
+        Integer expectedResult = 1;
+
+        when(accountRepository.deleteById(eq(id)))
+                .thenReturn(expectedResult);
+
+        Integer actualResult = accountService.deleteById(id);
+
+        assertEquals(expectedResult, actualResult);
+
+        verify(accountRepository, atMostOnce()).deleteById(eq(id));
+    }
+
+    @Test
+    public void deleteByIdShouldReturn0AffectedRowsWhenFailedToDelete() {
+        String id = "aaaa-aaaa-aaaa";
+        Integer expectedResult = 0;
+
+        when(accountRepository.deleteById(eq(id)))
+                .thenReturn(expectedResult);
+
+        assertThrows(RuntimeException.class, () -> {
+            accountService.deleteById(id);
+        });
+
+        verify(accountRepository, atMostOnce()).deleteById(eq(id));
+    }
+
+    @Test
+    public void updateBalanceShouldReturnAccountOnSuccess() {
+        Account expectedResult = Account.builder()
+                .accountId("f048c183-066c-4906-a0be-e5fe08dccd6b")
+                .balance(BigDecimal.valueOf(10_000.00))
+                .alias("New Account").build();
+
+        when(accountRepository.updateBalance(eq(expectedResult)))
+                .thenReturn(Optional.of(expectedResult));
+
+        Account actualResult = accountService.updateBalance(expectedResult);
+
+        assertEquals(expectedResult, actualResult);
+
+        verify(accountRepository, atMostOnce())
+                .updateBalance(eq(expectedResult));
+    }
+
+    @Test
+    public void updateBalanceShouldThrowExceptionOnFailure() {
+        Account expectedResult = Account.builder()
+                .accountId("f048c183-066c-4906-a0be-e5fe08dccd6b")
+                .balance(BigDecimal.valueOf(10_000.0050))
+                .alias("New Account").build();
+
+        when(accountRepository.updateBalance(eq(expectedResult)))
+                .thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () ->
+                accountService.updateBalance(expectedResult)
+        );
+
+        verify(accountRepository, atMostOnce())
+                .updateBalance(eq(expectedResult));
     }
 }
