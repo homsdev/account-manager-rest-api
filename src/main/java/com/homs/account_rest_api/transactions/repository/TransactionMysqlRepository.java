d4a200e988c269e8927ffa1d2a0231274d1f5072
homsdev <obr_dev@protonmail.com>
2025-06-23 13:57:48 -0600

    fix: solved issue, month and year were always required params. And they should be optionals

--- src/main/java/com/homs/account_rest_api/transactions/repository/TransactionMysqlRepository.java
+++ src/main/java/com/homs/account_rest_api/transactions/repository/TransactionMysqlRepository.java
@@ -4,11 +4,13 @@ import com.homs.account_rest_api.transactions.mapper.TransactionRowMapper;
 import com.homs.account_rest_api.transactions.model.Transaction;
 import com.homs.account_rest_api.transactions.exceptions.TransactionInvalidData;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 
+import java.math.RoundingMode;
 import java.time.Month;
 import java.time.Year;
 import java.util.*;
@@ -17,6 +19,7 @@ import static com.homs.account_rest_api.utils.TransactionDataValidation.isValid;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class TransactionMysqlRepository implements TransactionRepository {
 
     @Value("${transaction.save}")
@@ -37,13 +40,14 @@ public class TransactionMysqlRepository implements TransactionRepository {
      * or an empty {@link Optional} if transaction was not saved
      */
     @Override
-    public Optional<Transaction> saveTransaction(final Transaction transaction){
+    public Optional<Transaction> saveTransaction(final Transaction transaction) {
 
         if (!Boolean.TRUE.equals(isValid(transaction))) throw new TransactionInvalidData("Missing transaction data");
-
+        log.info("Saving transaction into database");
+        log.info("amount: {}", transaction.getAmount().setScale(2, RoundingMode.HALF_UP));
         Map<String, Object> params = new HashMap<>();
         params.put("transactionId", transaction.getTransactionId());
-        params.put("amount", transaction.getAmount());
+        params.put("amount", transaction.getAmount().setScale(2, RoundingMode.HALF_UP));
         params.put("type", transaction.getType().getValue());
         params.put("date", transaction.getDate().toString());
         params.put("accountId", transaction.getAccount().getAccountId());
@@ -55,22 +59,22 @@ public class TransactionMysqlRepository implements TransactionRepository {
 
     /**
      * Retrieves all transactions by the given month and year
+     *
      * @param accountId {@link String}
-     * @param month {@link Month}
-     * @param year {@link Year}
+     * @param month     {@link Month}
+     * @param year      {@link Year}
      * @return
      */
     @Override
-    public List<Transaction> getAllTransactionsByMonth(String accountId,Month month, Year year) {
-        if(accountId.isEmpty()){
+    public List<Transaction> getAllTransactionsByMonth(String accountId, Month month, Year year) {
+        if (accountId.isEmpty()) {
             return Collections.emptyList();
         }
-        Map<String,Object> params = new HashMap<>();
-        params.put("accountId",accountId);
-        params.put("year",year.getValue());
-        params.put("month",month.getValue());
-
-        return jdbcTemplate.query(filterTransactionsByDate,params, new TransactionRowMapper());
+        Map<String, Object> params = new HashMap<>();
+        params.put("accountId", accountId);
+        params.put("year", year.getValue());
+        params.put("month", month.getValue());
+        return jdbcTemplate.query(filterTransactionsByDate, params, new TransactionRowMapper());
     }
 
 
