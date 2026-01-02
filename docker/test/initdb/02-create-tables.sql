caa1cd2fb74c92f8c267cc2e9b9b76dea1fe53e0
homsdev <obr_dev@protonmail.com>
2026-01-02 15:01:28 -0600

    feat: migration to graddle

--- docker/test/initdb/02-create-tables.sql
+++ docker/test/initdb/02-create-tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS cli_account(
     account_id VARCHAR(60) NOT NULL PRIMARY KEY,
     balance DECIMAL(10,2) NOT NULL DEFAULT 0.00 CHECK (balance >= 0),
     alias VARCHAR(60) NOT NULL    
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS cli_transaction(
     transaction_id VARCHAR(60) NOT NULL PRIMARY KEY,
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS cli_transaction(
     FOREIGN KEY (transaction_account) REFERENCES cli_account(account_id) 
     ON DELETE RESTRICT ON UPDATE CASCADE,
     INDEX idx_account_date (transaction_account, transaction_date)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS cli_category(
     category_id VARCHAR(60) NOT NULL PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
